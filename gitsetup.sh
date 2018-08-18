2fd12629281a5f445074c32efc9de6775f224fcd
gwhite <gazzawhite@gmail.com>
2018-08-18 08:28:55 +0200

    added git isntall

--- gitsetup.sh
+++ gitsetup.sh
@@ -1,6 +1,9 @@
 ## setup variables for git commit
 # usage: sh gitsetup.sh <email address> 
 
+sudo apt update
+sudo apt install git
+
 git config --global user.email "$1"
 git config --global user.name "$USER"
 
