859385430124eb02ec1812f446c6f04056bd4320
gwhite <gazzawhite@gmail.com>
2018-08-18 08:22:52 +0200

    adding some info for usage

--- gitsetup.sh
+++ gitsetup.sh
@@ -1,2 +1,6 @@
-  git config --global user.email "$1"
-  git config --global user.name "$USER"
+## setup variables for git commit
+# usage: sh gitsetup.sh <email address> 
+
+git config --global user.email "$1"
+git config --global user.name "$USER"
+
